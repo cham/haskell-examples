b161f6a672b1ad62205f99cf0a5b2f89dd004397
Dan Neame <danneame@gmail.com>
2014-08-02 23:40:03 +0100

    Moved to chapter1 directory

---
+++ chapter1/intro-to-lists.hs
@@ -0,0 +1,12 @@
+lostNumbers = [4,8,15,16,23,42]
+moreLostNumbers = 5:lostNumbers
+thirdLostNumber = lostNumbers !! 2
+
+b = [[1,2,3,4],[5,3,3,3],[1,2,2,3,4],[1,2,3]]
+c = b ++ [[1,1,1,1,1]]
+d = b !! 2
+
+greaterThan = [3,2,1] > [2,1,0]
+alsoGreaterThan = [3,2,1] > [2,10,10000000]
+lessThan = [3,4,2] < [3,4,3]
+areEqualLists = [3,4,2] == [3,4,2]
