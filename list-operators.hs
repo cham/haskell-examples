afd273e43d975e2fb8b060b216fe323d2c1c7044
Dan Neame <danneame@gmail.com>
2014-08-02 22:53:02 +0100

    List operators examples

---
+++ list-operators.hs
@@ -0,0 +1,15 @@
+headTailList = [5,4,3,2,1]
+theHead = head headTailList
+theTail = tail headTailList
+theLast = last headTailList
+theInit = init headTailList
+theReversed = reverse headTailList
+theHeadThreeLong = take 3 headTailList
+theHeadSixLong = take 6 headTailList
+theTailThreeLong = drop 3 headTailList
+theLargestNumber = maximum headTailList
+theSmallestNumber = minimum headTailList
+theSum = sum headTailList
+theProduct = product headTailList
+theListHasFive = elem 5 headTailList
+theListHasEight = 8 `elem` headTailList
