aae95836938829c12ec472b1a2ddb936b40d5791
Dan Neame <danneame@gmail.com>
2014-08-03 12:42:21 +0100

    Pattern matching lists examples

---
+++ chapter3-function-syntax/3-matchinglists.hs
@@ -0,0 +1,9 @@
+head' :: [a] -> a
+head' [] = error "Can't get the head of an empty list!"
+head' (x:_) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty!"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
